77c94e639b048e63913d586bcfbd1f9f45d5270b
vaik-info <info@vaikobo.com>
2022-10-24 22:36:50 +0900

    first commit

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "bastion_ec2_key_pair" {
-  description = "Bastion EC2 Key Pair"
+output "ec2_key_pair" {
+  description = "EC2 Key Pair"
   value       = aws_instance.ec2.key_name
 }
 
-output "bastion_ec2_eip" {
-  description = "Bastion EC2 EIP"
+output "ec2_eip" {
+  description = "EC2 EIP"
   value       = aws_eip.ec2-eip.public_ip
 }
