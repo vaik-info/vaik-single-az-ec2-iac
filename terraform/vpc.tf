5d59c86ff4081c53051998954002561c5d3e4adf
vaik-info <info@vaikobo.com>
2022-10-25 09:16:56 +0900

    first commit

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,10 +5,9 @@ module "vpc" {
   name                 ="${var.prefix}instance"
   cidr                 = "10.0.0.0/16"
   azs                  = ["${var.target_region}a"]
-  private_subnets      = ["10.0.1.0/24"]
   public_subnets       = ["10.0.4.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_nat_gateway   = false
+  single_nat_gateway   = false
   enable_dns_hostnames = true
 
   tags = {
