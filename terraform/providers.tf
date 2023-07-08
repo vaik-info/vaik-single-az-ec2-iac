a4d74dcbdbb76430fd704e8234ddf8449fe7a79a
k-wakisaka <k.wakisaka@vaikobo.com>
2023-07-08 10:06:00 +0900

    fix

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,4 @@
 provider "aws" {
   region = var.target_region
+  version = "~> 5.0"
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,5 @@
 module "vpc" {
   source  = "registry.terraform.io/terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
 
   name                 ="${var.prefix}instance"
   cidr                 = "10.0.0.0/16"
@@ -9,7 +8,6 @@ module "vpc" {
   enable_nat_gateway   = false
   single_nat_gateway   = false
   enable_dns_hostnames = true
-
   tags = {
     Terraform = "true"
     Environment = "dev"
