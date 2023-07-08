2ddbe46a275fb6593a2fbb8c49c5c8ee765c4936
k-wakisaka <k.wakisaka@vaikobo.com>
2023-07-08 09:34:33 +0900

    fix

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.2.6"
+  required_version = "== 1.2.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.56"
+      version = "== 3.56"
     }
   }
 
